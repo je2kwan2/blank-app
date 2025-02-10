c9de723fcfdca46f76189a378e28db0e67f643fd
memebena <je2kwan2@gmail.com>
2025-02-10 23:50:28 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
-
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
@@ -10,10 +13,7 @@ st.write("hi, hello world")
 st.sidebar.success("Select a demo above.")
 
 
-st.set_page_config(
-    page_title="Hello",
-    page_icon="👋",
-)
+
 
 st.write("# Welcome to Streamlit! 👋")
 
